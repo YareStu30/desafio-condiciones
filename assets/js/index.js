b9dedbd7c7cf9db84fdeda8b5dacf0002dd3da88
yarenla <yare.cs22@gmail.com>
2022-07-09 00:23:45 -0400

    add html 1,2,3

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+/* desafio number one */
+document.querySelector("#button").onclick = function border() {
+  if (button.style.border === "none") {
+    button.style.border = "solid 2px red";
+  } else {
+    button.style.border = "none";
+  }
+};
+
+/*desafio number two*/
+let verificacion = document.querySelector("#verificacion");
+verificacion.addEventListener("click", (e) => {
+  let logo1 = document.querySelector("#input1").value;
+  let logo2 = document.querySelector("#input2").value;
+  let logo3 = document.querySelector("#input3").value;
+  let total = Number(logo1) + Number(logo2) + Number(logo3);
+  if (total > 10) {
+    document.querySelector("#verificar").innerHTML =
+      "Excedes la cantidad de stickers";
+  } else {
+    document.querySelector("#verificar").innerHTML =
+      "Llevas" + " " + total + " " + "items";
+  }
+  console.log("#input1");
+  console.log(total);
+});
+
+/*desafio number 3*/
